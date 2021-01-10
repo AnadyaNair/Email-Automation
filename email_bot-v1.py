5367aa54fe9c648ce6e744d26a7b97ca472a0c0c
AnadyaNairofficial✔ <71205867+AnadyaNair@users.noreply.github.com>
2021-01-10 20:19:03 +0530

    Automatic Email bot Python version 1

--- email_bot-v1.py
+++ email_bot-v1.py
@@ -4,7 +4,7 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-passpasscode = "SENDER'S PASSWORD"
+passpasscode = "SENDER'S PASSWORD" # for security purpose, the passcode has to be a variable.
 server.login('SENDER'S EMAIL ADDRESS', passpasscode)
 server.sendmail('SENDER'S EMAIL ADDRESS', 
     'RECIEVER'S EMAIL ADDRESS',
